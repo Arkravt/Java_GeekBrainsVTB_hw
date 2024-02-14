c8a44ad8a0622ba3343160c49bc29bd06b618c90
Kravtsev Artem <arkravt@gmail.com>
2024-02-14 16:54:03 +0400

    add lesson5

---
+++ src/main/java/com/geekbrains/lesson5/MainApp.java
@@ -0,0 +1,36 @@
+package com.geekbrains.lesson5;
+
+import java.util.*;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        task1();
+
+//        PhoneBook pBook = new PhoneBook();
+//        pBook.add("Ivanov", 11111);
+//        pBook.add("Sidorov", 22222);
+//        pBook.add("Ahmed", 33333);
+//        pBook.add("Walker", 44444);
+//        pBook.add("Ivanov", 555555);
+//
+//        pBook.get("Ivanov");
+    }
+
+    public static void task1() {
+        String[] array = {"A", "B", "C", "D", "E", "F", "A", "A", "B", "C", "Z", "H"};
+        Map<String, Integer> map = new HashMap<>();
+
+
+        for (String elem : array) {
+            int count = map.get(elem) == null ? 0 : map.get(elem);
+            map.put(elem, ++count);
+        }
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+
+    }
+
+}
---
+++ src/main/java/com/geekbrains/lesson5/PhoneBook.java
@@ -0,0 +1,16 @@
+package com.geekbrains.lesson5;
+
+import java.util.*;
+
+public class PhoneBook {
+
+    private Map<String, HashSet<Integer>> phoneBook = new HashMap<>();
+
+    public void add(String LastName, int phoneNumber) {
+
+    }
+
+    public void get(String lastName) {
+
+    }
+}
