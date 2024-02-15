763fdfc47e350aee171d7491b50c29e6ab01b857
Kravtsev Artem <arkravt@gmail.com>
2024-02-15 20:13:59 +0400

    edit lesson6

--- src/main/java/com/geekbrains/lesson6/MainApp.java
+++ src/main/java/com/geekbrains/lesson6/MainApp.java
@@ -1,36 +1,33 @@
-package com.geekbrains.lesson6.solution2;
+package com.geekbrains.lesson6;
+
+import java.util.Arrays;
 
 public class MainApp {
 
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
-    static float[] arr;
+    static float[] arr = new float[SIZE];
 
     public static void main(String[] args) {
-        singleThread(getFilledArray());
-        try {
-            multiThread(getFilledArray());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        singleThread();
+        multiThread();
     }
 
 
-    static void singleThread(float[] arr) {
+    static void singleThread() {
         long start = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println("Single Thread: " + (System.currentTimeMillis() - start));
+
     }
 
-    static void multiThread(float[] arr) throws InterruptedException {
-
+    static void multiThread() {
+        Arrays.fill(arr, 1.0f);
         long start = System.currentTimeMillis();
-
         float[] arrHalf1 = new float[HALF];
         float[] arrHalf2 = new float[HALF];
-
         System.arraycopy(arr, 0, arrHalf1, 0, HALF);
         System.arraycopy(arr, HALF, arrHalf2, 0, HALF);
 
@@ -46,29 +43,24 @@ public class MainApp {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < arrHalf2.length; i++) {
-                    arrHalf2[i] = (float) (arrHalf2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                for (int i = 0, j = HALF; i < arrHalf2.length; i++, j++) {
+                    arrHalf2[i] = (float) (arrHalf2[i] * Math.sin(0.2f + j/ 5) * Math.cos(0.2f + j/ 5) * Math.cos(0.4f + j/ 2));
                 }
             }
         });
 
         t1.start();
         t2.start();
-        t1.join();
-        t2.join();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(arrHalf1, 0, arr, 0, HALF);
         System.arraycopy(arrHalf2, 0, arr, HALF, HALF);
-
         System.out.println("Multi Thread: " + (System.currentTimeMillis() - start));
-
-    }
-
-    static float[] getFilledArray() {
-        arr = new float[SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.0f;
-        }
-        return arr;
     }
 }
