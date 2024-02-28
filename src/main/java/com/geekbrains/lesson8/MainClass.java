d03934f3cc2d03fa8d4ade0a1a5a6765f876e287
Kravtsev Artem <arkravt@gmail.com>
2024-02-28 17:02:58 +0400

    edit lesson8

--- src/main/java/com/geekbrains/lesson8/MainClass.java
+++ src/main/java/com/geekbrains/lesson8/MainClass.java
@@ -1,9 +1,22 @@
 package com.geekbrains.lesson8;
 
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MainClass {
     public static void main(String[] args) {
 
+        // Task 1
+        String[] array = {"winter", "summer", "spring", "autumn", "summer", "spring", "summer"};
+        String result = Arrays.stream(array)
+                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
+
+        System.out.println(result);
+
     }
 }
-
--- src/main/java/com/geekbrains/lesson8/Testable.java
+++ src/main/java/com/geekbrains/lesson8/Testable.java
@@ -2,5 +2,5 @@ package com.geekbrains.lesson8;
 
 @FunctionalInterface
 public interface Testable {
-    void doSomeThing();
+    void doSomeThing(int peremA);
 }
