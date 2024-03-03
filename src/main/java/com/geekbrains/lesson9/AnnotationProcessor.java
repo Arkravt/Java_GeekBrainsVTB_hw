3659605b59a51d3cbf930d271d92cc1cf785154b
Kravtsev Artem <arkravt@gmail.com>
2024-03-03 23:30:22 +0400

    add lesson9

--- src/main/java/com/geekbrains/lesson9/AnnotationProcessor.java
+++ src/main/java/com/geekbrains/lesson9/AnnotationProcessor.java
@@ -2,6 +2,6 @@ package com.geekbrains.lesson9;
 
 public class AnnotationProcessor {
 
-    // Здесь будет рефлексия которая считаывает размечанный класс
+    // Здесь будет рефлексия которая считывает размечанный класс
 
 }
--- src/main/java/com/geekbrains/lesson9/MainClass.java
+++ src/main/java/com/geekbrains/lesson9/MainClass.java
@@ -1,48 +1,9 @@
 package com.geekbrains.lesson9;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainClass {
-
-    private static Connection connection;
-    private static Statement statement;
-
     public static void main(String[] args) {
-        try {
-            connect();
 
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
-
-
-    public static void connect() throws SQLException {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
-            statement = connection.createStatement();
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new SQLException("Enable to connect !");
-        }
-    }
-
-    public static void disconnect() {
-        try {
-            statement.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
