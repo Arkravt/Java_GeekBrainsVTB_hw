0ec8e444f4014b1ab36dfc5a6e40fe1854b718fb
Kravtsev Artem <arkravt@gmail.com>
2024-03-04 21:34:09 +0400

    edit lesson9

--- src/main/java/com/geekbrains/lesson9/AnnotationProcessor.java
+++ src/main/java/com/geekbrains/lesson9/AnnotationProcessor.java
@@ -1,8 +1,5 @@
 package com.geekbrains.lesson9;
 
-import java.awt.*;
-import java.io.FilterOutputStream;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.Arrays;
@@ -12,15 +9,13 @@ public class AnnotationProcessor {
 
     private static Connection connection;
     private static Statement statement;
-    private static PreparedStatement preparedStatement;
-    private static String DB_URL = "jdbc:sqlite:D:\\Study\\Java\\SQLite\\school.db";
+    private static String DB_URL = "jdbc:sqlite:C:\\Курсы\\Java\\SQLite\\school.db";
 
     private static void connect() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection(DB_URL);
             statement = connection.createStatement();
-            preparedStatement = connection.prepareStatement("INSERT INTO ? (?) VALUES (?)");
         } catch (ClassNotFoundException | SQLException e) {
             throw new SQLException("Enable to connect !");
         }
@@ -51,7 +46,7 @@ public class AnnotationProcessor {
             String sql = Arrays.stream(inputClass.getDeclaredFields())
                     .filter(f -> f.isAnnotationPresent(Column.class))
                     .map(f -> f.getName() + " " + f.getAnnotation(Column.class).type())
-                    .collect(Collectors.joining(",", "CREATE TABLE " + tableAnotation.title() + " (", ");"));
+                    .collect(Collectors.joining(",", "CREATE TABLE IF NOT EXISTS " + tableAnotation.title() + " (", ");"));
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -62,36 +57,42 @@ public class AnnotationProcessor {
 
     public static void addObjectToDataBase(Object objectToAdd) {
 
-        Class objectClass = objectToAdd.getClass();
+        Class<?> objectClass = objectToAdd.getClass();
 
         if (!objectClass.isAnnotationPresent(Table.class)) {
             return;
         }
 
-        Table tableAnotation = (Table) objectClass.getAnnotation(Table.class);
-        //String prepareSQL = "INSERT INTO " + tableAnotation.title() + " (";
+        try {
+            connect();
 
+            Table tableAnotation = (Table) objectClass.getAnnotation(Table.class);
 
-        Field[] fields = Arrays.stream(objectClass.getDeclaredFields())
-                .filter(f -> f.isAnnotationPresent(Column.class)).toArray(Field[]::new);
+            Field[] fields = Arrays.stream(objectClass.getDeclaredFields())
+                    .filter(f -> f.isAnnotationPresent(Column.class)).toArray(Field[]::new);
 
-        String prepareSQL = Arrays.stream(fields)
-                .map(f -> f.getName())
-                .collect(Collectors.joining(", ", "INSERT INTO " + tableAnotation.title() + "(", ")"));
+            String preparedSQL = Arrays.stream(fields)
+                    .map(Field::getName)
+                    .collect(Collectors.joining(", ", "INSERT INTO " + tableAnotation.title() + " (", ")"));
 
+            preparedSQL = Arrays.stream(fields)
+                    .map(f -> "?")
+                    .collect(Collectors.joining(", ", preparedSQL + " VALUES (", ");"));
 
+            PreparedStatement preparedStatement = connection.prepareStatement(preparedSQL);
 
+            for (int i = 0; i < fields.length; i++) {
+                fields[i].setAccessible(true);
+                preparedStatement.setObject(i + 1, fields[i].get(objectToAdd));
+            }
 
-//        try {
-//            connect();
-//
-//
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            disconnect();
-//        }
+            preparedStatement.execute();
+
+        } catch (IllegalAccessException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
+        }
 
     }
 
--- src/main/java/com/geekbrains/lesson9/MainClass.java
+++ src/main/java/com/geekbrains/lesson9/MainClass.java
@@ -4,10 +4,14 @@ import java.sql.*;
 
 public class MainClass {
     public static void main(String[] args) {
-        //AnnotationProcessor.createTable(Student.class);
+        AnnotationProcessor.createTable(Student.class);
 
         AnnotationProcessor.addObjectToDataBase(
                 new Student(1, "Petr", 16, 10)
+        );
+
+        AnnotationProcessor.addObjectToDataBase(
+                new Student(2, "Ivan", 10, 4)
         );
 
     }
