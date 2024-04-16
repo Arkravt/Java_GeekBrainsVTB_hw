b137a1d95e014e7b25f1590ad0194718197e2944
Kravtsev Artem <arkravt@gmail.com>
2024-04-17 00:27:24 +0400

    17.04.2024

---
+++ src/main/java/com/geekbrains/lesson14_Spring_Boot_Security/BootApp.java
@@ -0,0 +1,11 @@
+package com.geekbrains.lesson14_Spring_Boot_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApp.class, args);
+    }
+}
---
+++ src/main/resources/lesson14/db/migration/V1__init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled boolean NOT NULL,
+    PRIMARY KEY (USERNAME)
+);
+
+INSERT INTO users
+VALUES
+('user1', '{noop}123', true),
+('user2', '{noop}123', true);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+
+    CONSTRAINT authorities_idx UNIQUE (username, authority),
+
+    CONSTRAINT authorities_ibfk_1
+    FOREIGN KEY (username)
+    REFERENCES users (username)
+);
+
+INSERT INTO authorities
+VALUES
+('user1', 'ROLE_ADMIN'),
+('user2', 'ROLE_USER'),
+('user2', 'ROLE_USER');
+
+
+
